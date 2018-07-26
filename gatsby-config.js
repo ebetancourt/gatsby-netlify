56c2684212f049cfbc5eadcb4802dc9d22770689
Elliot Betancourt <elliot@elliotbetancourt.com>
2018-07-27 00:04:09 +0200

    remoe prefix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: 'Gatsby Default Starter',
     description: 'A new blog site'
   },
-  pathPrefix: '/new-gatsby',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components', {
